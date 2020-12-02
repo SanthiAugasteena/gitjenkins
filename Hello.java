39ea56884383a24afe853e8518e867c00b9db26b
SanthiAugasteena <santhi.augasteena-dasari@capgemini.com>
2020-12-02 12:22:39 +0530

    add Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{ 
+
+   public static void main(String[] ardgs){
+
+        for(int i=1;i<=10;i++){
+     
+            System.out.println("Hiiiii.... Hello..."+i);
+
+      }
+   }
+}
